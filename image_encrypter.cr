a98a3814af8a399e30ca8e287967fd3c11556182
Isaac Sloan <isaac@isaacsloan.com>
2018-11-14 17:12:32 -0800

    removed files for now

--- image_encrypter.cr
+++ image_encrypter.cr
@@ -35,33 +35,33 @@ end
 
 enc_data = simple_enc(data, "much password so secret".to_slice)
 
-File.open("#{filename}_simple.bmp", "w") do |f|
+File.open("#{filename}_1_simple.bmp", "w") do |f|
   f.write headers
   f.write(enc_data)
 end
 
-File.open("#{filename}_simpleunen.bmp", "w") do |f|
+File.open("#{filename}_2_simpleunenc.bmp", "w") do |f|
   f.write headers
   f.write simple_enc(enc_data, "much password so secret".to_slice)
 end
 
-File.open("#{filename}_ecb.bmp", "w") do |f|
+File.open("#{filename}_3_ecb.bmp", "w") do |f|
   f.write headers
   f.write encrypt(data, "aes-256-ecb", "much password so secret")
 end
 
-File.open("#{filename}_cbc.bmp", "w") do |f|
+File.open("#{filename}_4_cbc.bmp", "w") do |f|
   f.write headers
   f.write encrypt(data, "aes-256-cbc", "much password so secret")
 end
 
-File.open("#{filename}_ctr.bmp", "w") do |f|
+File.open("#{filename}_5_ctr.bmp", "w") do |f|
   f.write headers
   f.write encrypt(data, "aes-256-ctr", "much password so secret")
 end
 
 
-File.open("#{filename}_gcm.bmp", "w") do |f|
+File.open("#{filename}_6_gcm.bmp", "w") do |f|
   f.write headers
   f.write encrypt(data, "aes-256-gcm", "much password so secret")
 end
